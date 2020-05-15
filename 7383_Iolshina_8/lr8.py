c2b79701a16b7dd23f8aa6132addcb98c148c185
welarie <valeriya.iolshina@gmail.com>
2020-05-15 19:31:37 +0300

    lr8 done

---
+++ 7383_Iolshina_8/lr8.py
@@ -0,0 +1,73 @@
+import numpy
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint, callbacks
+
+
+filename = "wonderland.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
+
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+    seq_in = raw_text[i:i + seq_length]
+    seq_out = raw_text[i + seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+print("Total Patterns: ", n_patterns)
+
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+X = X / float(n_vocab)
+y = np_utils.to_categorical(dataY)
+
+class Callback(callbacks.Callback):
+    def __init__(self, epochs):
+        super(Callback, self).__init__()
+        self.epochs = epochs
+
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch in self.epochs:
+            generate_sequence(self.model)
+
+def generate_sequence(model):
+    start = numpy.random.randint(0, len(dataX) - 1)
+    pattern = dataX[start]
+    print("Seed:")
+    print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+    for i in range(1000):
+        x = numpy.reshape(pattern, (1, len(pattern), 1))
+        x = x / float(n_vocab)
+        prediction = model.predict(x, verbose=0)
+        index = numpy.argmax(prediction)
+        result = int_to_char[index]
+        print(result, end='')
+        pattern.append(index)
+        pattern = pattern[1:len(pattern)]
+
+
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint, Callback([1, 9, 18])]
+
+model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
